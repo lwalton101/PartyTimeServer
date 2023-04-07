8d39dec5ebdcb6783eb261c16946be9f70e02a9d
lwalton101 <waltonfamily01@outlook.com>
2023-04-07 11:54:54 +0100

    Created project structure

--- public/index.js
+++
@@ -1,7 +0,0 @@
-ws = new WebSocket("ws://" + window.location.hostname + ":3001")
-
-let sendButton = document.getElementById("sendButton");
-
-function buttonPress(){
-    ws.send((new Date() / 1000).toString())
-}
